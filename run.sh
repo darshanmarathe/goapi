cd5df3790ec8b8ece4bed8d8f6a92d5d5c9b2a99
Darshan Marathe <darshanmarathe@gmail.com>
2024-08-05 10:23:26 +0000

    first commit

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cls 
+clear
 go run .
---
+++ setEnv.sh
@@ -0,0 +1,8 @@
+clear 
+export APP_DB_USERNAME=ykqeaogj
+export APP_DB_PASSWORD=ZeEjD72l6u0rNoL0uCdrygBijRc_SpPH
+export APP_DB_NAME=ykqeaogj
+export APP_PORT=8000
+export APP_DB_HOST=rosie.db.elephantsql.com
+export APP_CONNECTIONSTRING=postgres://ykqeaogj:ZeEjD72l6u0rNoL0uCdrygBijRc_SpPH@rosie.db.elephantsql.com/ykqeaogj
+go run .
